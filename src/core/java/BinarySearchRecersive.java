43168ccc7e0113a5df21fa9f02623324f24995f9
Sree <srinivasareddybandi08@gmail.com>
2024-12-02 08:54:31 +0530

    binary search recersive

--- src/core/java/BinarySearchRecersive.java
+++ src/core/java/BinarySearchRecersive.java
@@ -4,10 +4,13 @@ public class BinarySearchRecersive {
 
 	public static void main(String[] args) {
 		int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 16, 17, 19, 20 };
-		int target = 15;
-		int findElemt = findElemt(nums, 0, nums.length - 1, target);
-		System.out.println("Element found at index : " + findElemt);
-
+		int target = 100;
+		int result = findElemt(nums, 0, nums.length - 1, target);
+		if (result != -1) {
+			System.out.println("Element found at index : " + result);
+		} else {
+			System.out.println("Not Found..");
+		}
 	}
 
 	static int findElemt(int[] nums, int left, int right, int target) {
